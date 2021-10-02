1f675e301cfd6633b09f00380db86f5ff3303472
saucesar <sau.cesarlima2013@gmail.com>
2021-10-02 03:58:15 -0300

    definição de nome da tabela e dos atributos.

--- src/models/time_clock_marking.py
+++ src/models/time_clock_marking.py
@@ -1,5 +1,12 @@
 from _imports import *
 
 class TimeClockMarking:
+    
+    table = 'time_clock_markings'
+    attributes = ('date','time', 'pis')
+
     def __init__(self) -> None:
         pass
+
+if __name__ == "__main__":
+    print('TimeClockMarking')
