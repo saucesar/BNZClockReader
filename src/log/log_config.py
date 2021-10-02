eee1242bb8f4a57c35c379629bde43b6cf316528
saucesar <sau.cesarlima2013@gmail.com>
2021-10-02 03:56:42 -0300

    Configuração propria para logs.

---
+++ src/log/log_config.py
@@ -0,0 +1,3 @@
+import logging
+#logging.basicConfig(filename='dev.log', format='%(levelname)s %(asctime)s : %(message)s', level=logging.DEBUG)
+logging.basicConfig(format='%(levelname)s %(asctime)s : %(message)s', level=logging.DEBUG)
