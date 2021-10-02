eeaf54cde0b45f33b323ef0ee3f00523a5138421
saucesar <sau.cesarlima2013@gmail.com>
2021-10-02 03:21:49 -0300

    Modificação de pasta

--- src/read_afd.py
+++ src/read_afd.py
@@ -1,7 +1,12 @@
-from src.models.employee import Employee
+from models.employee import Employee
+import logging
+
+logging.basicConfig(format='%(levelname)s %(asctime)s : %(message)s', level=logging.DEBUG)
 
 file_name = "/home/cesars/Dropbox/Arquivos/Projects/BNZClockPoint/AFD00009003650016557.txt"
 file = open(file_name, 'r')
+pis = "20753507018"
+logging.info("OPEN FILE: "+file_name)
 count = 0
 for line in file.readlines():
     if(line.__len__() > 35):
@@ -9,12 +14,13 @@ for line in file.readlines():
         name = line[35:].strip()
             
         if not name.isnumeric():
-            print(pis, name, name.__len__())
-    elif line.__len__() == 35 and "20753507018" in line:
+            Employee.create(name, pis)
+    if line.__len__() == 35 and pis in line:
         date = line[10:12]+"/"+line[12:14]+"/"+line[14:18]
         time = line[18:20]+":"+line[20:22]
         print(date, time)
         if(count == 10):
             break
-
+    count+=1
 file.close()
+logging.info("CLOSE FILE: "+file_name)
