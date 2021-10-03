289fe3c205376865d092ba2b649cd4460e7da302
saucesar <sau.cesarlima2013@gmail.com>
2021-10-03 14:45:27 -0300

    Tabela para salvar elementos chave-valor para configuração.

--- src/database/create_tables.py
+++ src/database/create_tables.py
@@ -3,12 +3,12 @@ import logging
 from os.path import dirname, join, abspath
 sys.path.insert(0, abspath(join(dirname(__file__), '..')))
 sys.path.insert(0, abspath(join(dirname(__file__), '../models')))
-from models.employee import Employee
-from models.time_clock_marking import TimeClockMarking
+from models._models import *
 
 logging.info("START CREATING TABLES")
 
 Employee.create_table()
 TimeClockMarking.create_table()
+KeyValue.create_table()
 
 logging.info("END CREATING TABLES")
