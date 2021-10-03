0d3c383b763b0fda174e2a290eaa09fc05530704
saucesar <sau.cesarlima2013@gmail.com>
2021-10-02 21:44:41 -0300

    ajuste nos imports padrão

--- src/models/_imports.py
+++ src/models/_imports.py
@@ -2,4 +2,4 @@ import sys
 from os.path import dirname, join, abspath
 sys.path.insert(0, abspath(join(dirname(__file__), '..')))
 from datetime import datetime
-from database.connection import Connection
+from _model import *
