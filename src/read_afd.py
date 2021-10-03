b958cc0ea9c202a9256808f189c54b0a57382f53
saucesar <sau.cesarlima2013@gmail.com>
2021-10-03 18:34:25 -0300

    funcoes de leitura implementadas com orientação a objetos

--- src/read_afd.py
+++ src/read_afd.py
@@ -1,39 +1,99 @@
 import sys
-import peewee
 from datetime import datetime
+from os import system
 from os.path import dirname, join, abspath
 sys.path.insert(0, abspath(join(dirname(__file__), 'models')))
+sys.path.insert(0, abspath(join(dirname(__file__), 'exceptions')))
 from models._models import *
+from exceptions.peewee_exceptions import *
 from log.log_config import *
 
-file_name = "/home/cesars/Dropbox/Arquivos/Projects/BNZClockPoint/AFD00009003650016557.txt"
-file = open(file_name, 'r')
-pis_search = "  "
-logging.info("OPEN FILE: "+file_name)
-
-for line in file.readlines():
-
-    if(line.__len__() > 35):
+class ReadAFDFile:
+    def __init__(self, afd_file_path) -> None:
+        self.file_path = afd_file_path
         
-        name = line[35:].strip()
-        pis = line[24:35]
-
-        if not name.isnumeric() and pis.isnumeric():
-            try:
-                Employee.create(name=name, pis=pis)
-            except peewee.IntegrityError as e:
-                logging.error(e)
-
-    if line.__len__() == 35:
-        pis = line[23:34]
-        date = line[14:18]+"/"+line[12:14]+"/"+line[10:12]
-        time = line[18:20]+":"+line[20:22]
-
+    def open_afd_file(self):
         try:
+            self.afd_file = open(self.file_path, 'r')
+            logging.info("OPEN FILE: "+self.file_path)
+        except Exception as e:
+            logging.error('EXCEPTION: '+ e.__class__.__name__+' MESSAGE:  '+e.__str__())
+
+    def close_afd_file(self):
+        try:
+            self.afd_file.close()
+            logging.info("CLOSE FILE: "+self.file_path)
+        except Exception as e:
+            logging.error('EXCEPTION: '+ e.__class__.__name__+' MESSAGE:  '+e.__str__())
+
+    def get_afd_lines_key_value(self):
+        try:
+            return KeyValue.get(KeyValue.key == KeyValue.AFD_LINES_READED_COUNT_KEY)
+        except:
+            kv = KeyValue.create(key=KeyValue.AFD_LINES_READED_COUNT_KEY, value='0')
+            logging.info('CREATED LINES_READED')
+            print('LINES',kv)
+            return kv
+    
+    def create_employee(self, line):
+        try:
+            name = line[35:].strip()
+            pis = line[24:35]
+
+            if not name.isnumeric() and pis.isnumeric():
+                Employee.create(name=name, pis=pis)
+
+            return True
+        except Exception as e:
+            logging.error('EXCEPTION: '+ e.__class__.__name__+' MESSAGE:  '+e.__str__())
+            return False
+
+    def create_time_clock_marking(self, line):
+        try:
+            pis = line[23:34]
+            date = line[14:18]+"/"+line[12:14]+"/"+line[10:12]
+            time = line[18:20]+":"+line[20:22]
+
             e = Employee.get(Employee.pis==pis)
             TimeClockMarking.create(time=time,date=datetime.strptime(date, '%Y/%m/%d'), pis=pis, employee=e)
-        except:
-            print('EXCEPTION')
 
-file.close()
-logging.info("CLOSE FILE: "+file_name)
+            return True
+        except Exception as e:
+            logging.error('EXCEPTION: '+ e.__class__.__name__+' MESSAGE:  '+e.__str__())
+            return False
+    
+    def print_percent_of_read_file(self, percent,):
+        sys.stdout.write("PERCENT FILE READED: "+str(percent)+"% "+("="*(int(percent))))
+        system('clear')
+        sys.stdout.flush()
+
+    def read_and_save_in_database(self):
+        self.open_afd_file()
+        file_line_amount = self.afd_file.readlines().__len__()
+        percent_count = 0
+        kv_lines = self.get_afd_lines_key_value()
+        lines_count = int(kv_lines.value)
+
+        self.afd_file.seek(0)
+
+        for line in self.afd_file.readlines()[lines_count:]:
+            lines_count += 1
+            percent_count += 1
+            self.print_percent_of_read_file(round(percent_count/file_line_amount, 3))
+            try:
+                kv_lines.value = lines_count
+                kv_lines.save()
+            except Exception as e:
+                logging.error('EXCEPTION: '+ e.__class__.__name__+' MESSAGE:  '+e.__str__())
+
+            if(line.__len__() > 35):
+                self.create_employee(line) 
+
+            if line.__len__() == 35:
+                self.create_time_clock_marking(line)
+
+        self.close_afd_file()
+
+if __name__ == '__main__':
+    readADF = ReadAFDFile('/home/cesars/Dropbox/Arquivos/Projects/BNZClockPoint/AFD00009003650016557.txt')
+    readADF.read_and_save_in_database()
