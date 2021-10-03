9f1c200b2ce09064586ac51fa03fa4c3c3ff06b8
saucesar <sau.cesarlima2013@gmail.com>
2021-10-03 18:32:48 -0300

    remoção de linha duplicada

--- src/models/_imports.py
+++ src/models/_imports.py
@@ -1,6 +1,5 @@
 import sys
 from os.path import dirname, join, abspath
-from os.path import dirname, join, abspath
 sys.path.insert(0, abspath(join(dirname(__file__), '..')))
 sys.path.insert(0, abspath(join(dirname(__file__), 'log')))
 from log.log_config import *
