34d6113ad50e3928db0b7b3c812aafb76806e5a9
saucesar <sau.cesarlima2013@gmail.com>
2021-10-02 21:45:00 -0300

    Model base com ORM

---
+++ src/models/_model.py
@@ -0,0 +1,10 @@
+from peewee import Model as BaseModel
+from peewee import SqliteDatabase as Connection
+from peewee import CharField as StringColumn
+from peewee import TimestampField as TimestampColumn
+from peewee import TimeField as TimeColumn
+from peewee import PrimaryKeyField as PrimaryKeyColumn
+
+class Model(BaseModel):
+    class Meta:
+        database = Connection('db.sqlite3')
