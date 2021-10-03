e19e7c037a4ddc8547a908d53c7f2b9762f9a1f9
saucesar <sau.cesarlima2013@gmail.com>
2021-10-02 22:23:18 -0300

    Adicao da tabela TimeClockMarking

--- src/database/create_tables.py
+++ src/database/create_tables.py
@@ -4,9 +4,11 @@ from os.path import dirname, join, abspath
 sys.path.insert(0, abspath(join(dirname(__file__), '..')))
 sys.path.insert(0, abspath(join(dirname(__file__), '../models')))
 from models.employee import Employee
+from models.time_clock_marking import TimeClockMarking
 
 logging.info("START CREATING TABLES")
 
 Employee.create_table()
+TimeClockMarking.create_table()
 
 logging.info("END CREATING TABLES")
