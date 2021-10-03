881607e631cb9ebb96c6cb62c4df579641383337
saucesar <sau.cesarlima2013@gmail.com>
2021-10-02 22:23:44 -0300

    Chave estrangeira habilitada no sqlite

--- src/models/_model.py
+++ src/models/_model.py
@@ -3,8 +3,9 @@ from peewee import SqliteDatabase as Connection
 from peewee import CharField as StringColumn
 from peewee import TimestampField as TimestampColumn
 from peewee import TimeField as TimeColumn
+from peewee import DateField as DateColumn
 from peewee import PrimaryKeyField as PrimaryKeyColumn
 
 class Model(BaseModel):
     class Meta:
-        database = Connection('db.sqlite3')
+        database = Connection('db.sqlite3', pragmas={'foreign_keys': 1})
