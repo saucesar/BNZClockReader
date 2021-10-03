68fb45d825af71c3e3c67f77ea9d928cf00f31f9
saucesar <sau.cesarlima2013@gmail.com>
2021-10-02 22:24:08 -0300

    configuração de log em arquivo

--- src/log/log_config.py
+++ src/log/log_config.py
@@ -1,3 +1,5 @@
 import logging
-#logging.basicConfig(filename='dev.log', format='%(levelname)s %(asctime)s : %(message)s', level=logging.DEBUG)
-logging.basicConfig(format='%(levelname)s %(asctime)s : %(message)s', level=logging.DEBUG)
+logging.basicConfig(filename='dev.log', format='%(levelname)s %(asctime)s : %(message)s', level=logging.DEBUG)
+#logging.basicConfig(format='%(levelname)s %(asctime)s : %(message)s', level=logging.DEBUG)
+logger = logging.getLogger('peewee')
+logger.addHandler(logging.StreamHandler())
