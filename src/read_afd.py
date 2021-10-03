e3ad92aed971e335136b2d128ab33a59400dcea9
saucesar <sau.cesarlima2013@gmail.com>
2021-10-03 14:46:50 -0300

    configuração para ler de arquivo e tranferir para o database

--- src/read_afd.py
+++ src/read_afd.py
@@ -1,27 +1,39 @@
 import sys
+import peewee
+from datetime import datetime
 from os.path import dirname, join, abspath
 sys.path.insert(0, abspath(join(dirname(__file__), 'models')))
-from models.employee import Employee
+from models._models import *
 from log.log_config import *
 
 file_name = "/home/cesars/Dropbox/Arquivos/Projects/BNZClockPoint/AFD00009003650016557.txt"
 file = open(file_name, 'r')
-pis = "20753507018"
+pis_search = "  "
 logging.info("OPEN FILE: "+file_name)
-count = 0
+
 for line in file.readlines():
+
     if(line.__len__() > 35):
-        pis = line[24:35]
+        
         name = line[35:].strip()
-            
-        if not name.isnumeric():
-            Employee.create(name, pis)
-    if line.__len__() == 35 and pis in line:
-        date = line[10:12]+"/"+line[12:14]+"/"+line[14:18]
+        pis = line[24:35]
+
+        if not name.isnumeric() and pis.isnumeric():
+            try:
+                Employee.create(name=name, pis=pis)
+            except peewee.IntegrityError as e:
+                logging.error(e)
+
+    if line.__len__() == 35:
+        pis = line[23:34]
+        date = line[14:18]+"/"+line[12:14]+"/"+line[10:12]
         time = line[18:20]+":"+line[20:22]
-        print(date, time)
-        if(count == 10):
-            break
-    count+=1
+
+        try:
+            e = Employee.get(Employee.pis==pis)
+            TimeClockMarking.create(time=time,date=datetime.strptime(date, '%Y/%m/%d'), pis=pis, employee=e)
+        except:
+            print('EXCEPTION')
+
 file.close()
 logging.info("CLOSE FILE: "+file_name)
