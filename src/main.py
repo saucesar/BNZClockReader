8344939119e0ff6588ec2b1d0973e5fb4b0821c0
sau cesar <sau.cesarlima2013@gmail.com>
2021-10-06 18:17:14 -0300

    adição de opção de gerar planilha de marcações

--- src/main.py
+++ src/main.py
@@ -14,25 +14,31 @@ class Main:
     def __init__(self) -> None:
         self.console = Console()       
         self.layout = Layout(name="Menu")
-        msg = "=========================================================\n 1 - Ler Arquide AFD\n99 - Sair\n========================================================="
+        msg = "1 - Ler Arquide AFD\n2 - Gerar Planilha de Marcações\n99 - Sair\n"
         options = Layout(name='Opções', renderable=self.create_panel_option(msg))
-        self.layout.split_row(options, Layout(name='Example'))
+        self.layout.split_row(options, Layout(name=''))
 
     def create_panel_option(self, option):
         return Panel(Text(option))
 
     def read_afd(self):
+        afd_path = input('Caminho do Arquivo AFD: ')
         self.console.log("[green]Lendo arquivo AFD\n")
-        readADF = ReadAFDFile('/home/cesars/Dropbox/Arquivos/Projects/BNZClockPoint/AFD00009003650016557.txt')
+        readADF = ReadAFDFile(afd_path)
         readADF.read_and_save_in_database()
         self.console.log('\n')
 
+    def create_spreadsheet(self):
+        month = input('Mês (de 1 a 12): ')
+        year =input('Ano (com 4 dígitos): ')
+        excel = Excel()
+        excel.save_month_db_spreadsheet(int(year), int(month))
+        
     def show(self):
         print(self.layout)
-        self.console.log("Menu!")
     
     def exit(self):
-        print(Panel('[red]Exit', title='Bye Bye!', subtitle='Come back again!'))
+        print(Panel('[red]Saindo!', title='Até breve!', subtitle='Volte sempre!'))
 
 if __name__ == '__main__':
     main = Main()
@@ -42,6 +48,8 @@ if __name__ == '__main__':
         option = input('Opção: ')
         if option == '1':
             main.read_afd()
+        if option == '2':
+            main.create_spreadsheet()
         if option == '99':
             main.exit()
             break
