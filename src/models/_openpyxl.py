ee4975a42d1621da33a3266415758bc53d4310f0
saucesar <sau.cesarlima2013@gmail.com>
2021-10-06 01:03:38 -0300

    Imports em arquivos separados.

---
+++ src/models/_openpyxl.py
@@ -0,0 +1,3 @@
+from openpyxl import Workbook
+from openpyxl.worksheet.table import Table, TableStyleInfo
+from openpyxl.styles import NamedStyle, Font, Border, Side, PatternFill
---
+++ src/models/_peewee_orm.py
@@ -0,0 +1,8 @@
+from peewee import Model as BaseModel
+from peewee import SqliteDatabase as Connection
+from peewee import CharField as StringColumn
+from peewee import TimestampField as TimestampColumn
+from peewee import TimeField as TimeColumn
+from peewee import DateField as DateColumn
+from peewee import PrimaryKeyField as PrimaryKeyColumn
+from peewee import ForeignKeyField as ForeignKeyColumn
