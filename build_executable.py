3d53679624efc84d80abcedc2b0601b9b709e724
sau cesar <sau.cesarlima2013@gmail.com>
2021-10-07 15:24:05 -0300

    ajuste para gerar executável windows

--- build_executable.py
+++ build_executable.py
@@ -3,7 +3,11 @@ import os
 class BuildExecutable:
     
     def __init__(self) -> None:
-        os.system('pyinstaller src/main.py src/database/create_tables.py src/read_afd.py src/models/*.py  --onefile')
+        if os.name == 'nt':
+            os.system('pyinstaller src/main.py src/database/create_tables.py src/read_afd.py '+\
+                      'src/models/_imports.py src/models/_models.py src/models/_openpyxl.py src/models/_peewee_orm.py --onefile')    
+        else:
+            os.system('pyinstaller src/main.py src/database/create_tables.py src/read_afd.py src/models/*.py  --onefile')
         #os.system('ls')
 
 if __name__ == '__main__':
