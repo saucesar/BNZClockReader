626db1ecab25c6c9c2d765ce64fa51add95c4c0c
saucesar <sau.cesarlima2013@gmail.com>
2021-10-07 00:07:00 -0300

    Orientação a objetos ao criar tabelas.

--- src/database/create_tables.py
+++ src/database/create_tables.py
@@ -5,10 +5,18 @@ sys.path.insert(0, abspath(join(dirname(__file__), '..')))
 sys.path.insert(0, abspath(join(dirname(__file__), '../models')))
 from models._models import *
 
-logging.info("START CREATING TABLES")
+class TableManager:
+    def __init__(self) -> None:
+        pass
 
-Employee.create_table()
-TimeClockMarking.create_table()
-KeyValue.create_table()
+    def create_tables(self):
+        logging.info("START CREATING TABLES")
 
-logging.info("END CREATING TABLES")
+        Employee.create_table()
+        TimeClockMarking.create_table()
+        KeyValue.create_table()
+
+        logging.info("END CREATING TABLES")
+
+if __name__ == '__main__':
+    TableManager().create_tables()
