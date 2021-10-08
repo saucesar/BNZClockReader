329765a64a403544891093930b8f9c49090faf52
saucesar <sau.cesarlima2013@gmail.com>
2021-10-08 01:18:14 -0300

    Aprimoramento no script de geração de executável.

--- build_executable.py
+++ build_executable.py
@@ -2,13 +2,19 @@ import os
 
 class BuildExecutable:
     
+    WINDOWS = 'nt'
+    LINUX = 'posix'
+    
     def __init__(self) -> None:
-        if os.name == 'nt':
+        if os.name == BuildExecutable.LINUX:
+            os.system('pip3 install pyinstaller')
+            os.system('pyinstaller src/main.py src/database/create_tables.py src/read_afd.py src/models/*.py -n ClockReader --clean --onefile')
+        if os.name == BuildExecutable.WINDOWS:
+            os.system('pip install pyinstaller')
             os.system('pyinstaller src/main.py src/database/create_tables.py src/read_afd.py '+\
-                      'src/models/_imports.py src/models/_models.py src/models/_openpyxl.py src/models/_peewee_orm.py --onefile')    
+                      'src/models/_imports.py src/models/_models.py src/models/_openpyxl.py src/models/_peewee_orm.py -n ClockReader --clean --onefile')    
         else:
-            os.system('pyinstaller src/main.py src/database/create_tables.py src/read_afd.py src/models/*.py  --onefile')
-        #os.system('ls')
+            os.system('echo "OS not supported."')
 
 if __name__ == '__main__':
     BuildExecutable()
