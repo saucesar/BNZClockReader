26cdc92fad9e1246360eda6f1a3b175e992ef37b
saucesar <sau.cesarlima2013@gmail.com>
2021-10-09 00:03:08 -0300

    Ajuste na imagem e progressbar aparecendo quando necessário

--- src/menu.py
+++ src/menu.py
@@ -1,14 +1,12 @@
-import sys, io
+import sys
 import PySimpleGUI as sg
 from os.path import dirname, join, abspath
 sys.path.insert(0, abspath(join(dirname(__file__), '..')))
 sys.path.insert(0, abspath(join(dirname(__file__), 'database')))
-from PySimpleGUI.PySimpleGUI import Slider
 from main import Main
 from facade import Facade
 from datetime import datetime
 from database.create_tables import TableManager
-from PIL import Image
 
 class MenuScreen:
     def __init__(self) -> None:
@@ -18,7 +16,6 @@ class MenuScreen:
         self.window = self.get_window()
         self.main = Main()
         self.facade = Facade()
-        self.load_image()
         TableManager().create_tables()
     
     def get_window(self):
@@ -34,13 +31,6 @@ class MenuScreen:
             if event == 'exit' or event == sg.WIN_CLOSED:
                 break
         self.window.close()
-    def load_image(self):
-        #image = Image.open('src/assets/exit3-96px.png')
-        image = Image.open('src/assets/clock-96.png')
-        image.thumbnail((400, 400))
-        bio = io.BytesIO()
-        image.save(bio, format="PNG")
-        self.window["image"].update(data=bio.getvalue())
 
     def get_layout(self):
         layout = [
@@ -49,7 +39,7 @@ class MenuScreen:
                 sg.Button('', key='createSpreadsheet', tooltip='Gerar Planilha', size=(20, 10), image_filename='src/assets/excel-96px.png'),
                 sg.Button('', key='exit', tooltip='Sair do Sistema', size=(20,10), image_filename='src/assets/exit2-96px.png'),
             ],
-            [ sg.Image(key="image")],
+            [ sg.Image(source='src/assets/clock2.png', size=(100,100),key="image", expand_x=True, expand_y=True)],
             [ sg.Text(text='- Geração planilhas', font=("Helvetica", 20)) ],
             [ sg.Text(text='- Calculo de horários', font=("Helvetica", 20)) ],
             [ sg.Text(text='- Erros de ponto', font=("Helvetica", 20)) ],
@@ -77,7 +67,8 @@ class ReadAFDScreen(MenuScreen):
         while True:
             event, values = self.window.read()
             if event == 'ok':
-                if values['afd_file'] != '': 
+                if values['afd_file'] != '':
+                    self.window['progressBar'].update(visible=True, current_count=0)
                     self.window.Element('progressText').update('Lendo arquivo... Aguarde...')
                     sg.popup_notify(title='Lendo arquivo, aguarde ...', display_duration_in_ms=1500)
                     self.facade.read_afd(values['afd_file'], self.window['progressBar'])
@@ -97,7 +88,7 @@ class ReadAFDScreen(MenuScreen):
                 sg.Text(text="Selecione o arquivo AFD: "),
                 sg.FileBrowse(button_text='Selecione',key='afd_file', tooltip='Arquivo AFD', ),
             ],
-            [sg.ProgressBar(100, key='progressBar', size=(20,20), bar_color=('green', 'gray'),)],
+            [sg.ProgressBar(100, key='progressBar', visible=False, size=(30,30), bar_color=('green', 'gray'),)],
             [sg.Text(text="", key='progressText')],
             [
                 self.oKbutton(btn_tooltype='Pressione ok Para ler o arquivo'),
@@ -107,6 +98,8 @@ class ReadAFDScreen(MenuScreen):
         return layout
 
 class CreateSpreadsheet(MenuScreen):
+    months = {'Janeiro':1, 'Fevereiro':2, 'Março':3, 'Abril':4, 'Maio':5, 'Junho':6, 'Julho':7,'Agosto':8,'Setembro':9,'Outubro':10,'Novembro':11, 'Dezembro':12}
+
     def __init__(self, main = None, facade = None) -> None:
         if not main is None and not facade is None:
             sg.theme('Tan')
@@ -122,21 +115,29 @@ class CreateSpreadsheet(MenuScreen):
         while True:
             event, values = self.window.read()
             if event == 'ok':
-                self.facade.create_spreadsheet(int(values['month']), int(values['year']), self.window['progressBar'])
-                sg.popup_notify(title='Planilha gerada', display_duration_in_ms=3000, location=(500,100))
+                try:
+                    year = int(values['year'][0])
+                    month = CreateSpreadsheet.months[values['month'][0]]
+                    self.window['progressBar'].update(visible=True, current_count=0)
+
+                    self.facade.create_spreadsheet(month, year, self.window['progressBar'])
+                    sg.popup_notify(title='Planilha gerada', display_duration_in_ms=3000, location=(500,100))
+                except:
+                    sg.popup_notify(title='Selecione o Mês e o Ano', location=(500, 200))
             if event == 'exit' or event == sg.WIN_CLOSED:
                 break
-
         self.window.close()
         
-
     def get_layout(self):
         date =  datetime.now()
+        months = ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro']
         layout = [
-            [sg.Text("O período",font=30)],
-            [sg.Text("Mês: "), Slider(range=(1, 12), default_value=date.month, key='month', orientation='h')],
-            [sg.Text("Ano: "), Slider(range=(2000, date.year), default_value=date.year, key='year', orientation='h')],
-            [sg.ProgressBar(100, key='progressBar', bar_color=('green', 'gray'), size=(20,20))],
+            [sg.Text("Selecione o período",font=('Times', 20))],
+            [
+                sg.Listbox(key='month',values= months, default_values=[months[date.month-1]], size=(20, 12)),
+                sg.Listbox(key='year',values=range(2018, date.year+1), default_values=[date.year,], size=(20, 12))
+            ],
+            [sg.ProgressBar(100, key='progressBar', visible=False, bar_color=('green', 'gray'), size=(30,30))],
             [
                 self.oKbutton(btn_tooltype='Pressione ok para gerar a planilha'),
                 self.exitButton(btn_tooltype='Sair desta tela.'),
@@ -145,5 +146,4 @@ class CreateSpreadsheet(MenuScreen):
         return layout
 
 if __name__ == '__main__':
-    menu = MenuScreen()
-    menu.show()
+    MenuScreen().show()
