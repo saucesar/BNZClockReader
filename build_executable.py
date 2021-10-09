aec91dd4fe544312186b6ea15a9965c6d6f4addc
saucesar <sau.cesarlima2013@gmail.com>
2021-10-09 00:12:07 -0300

    ajuste para build com asset para a GUI

--- build_executable.py
+++ build_executable.py
@@ -13,7 +13,7 @@ class BuildExecutable:
         
         if mode == '--gui':
             self.gui()
-        if mode == '--cli':
+        elif mode == '--cli':
             self.cli()
         else:
             print('Opção inválida')
@@ -23,10 +23,14 @@ class BuildExecutable:
         if os.name == BuildExecutable.LINUX:
             os.system('pip3 install pyinstaller')
             os.system('pyinstaller src/menu.py src/database/create_tables.py src/models/*.py -n ClockReader --clean --onefile')
-        if os.name == BuildExecutable.WINDOWS:
+            os.system('mkdir dist/src/')
+            os.system('cp -r src/assets dist/src/assets')
+        elif os.name == BuildExecutable.WINDOWS:
             os.system('pip install pyinstaller')
             os.system('pyinstaller src/menu.py src/database/create_tables.py src/models/_imports.py src/models/_models.py '+\
-                      'src/models/_openpyxl.py src/models/_peewee_orm.py -n ClockReader --clean --onefile')    
+                      'src/models/_openpyxl.py src/models/_peewee_orm.py -n ClockReader --clean --onefile')
+            os.system('mkdir dist/src/')
+            os.system('cp -r src/assets dist/src/assets')
         else:
             os.system('echo "OS not supported."')
 
@@ -34,7 +38,7 @@ class BuildExecutable:
         if os.name == BuildExecutable.LINUX:
             os.system('pip3 install pyinstaller')
             os.system('pyinstaller src/*.py src/database/*.py src/models/*.py -n ClockReader --clean --onefile')
-        if os.name == BuildExecutable.WINDOWS:
+        elif os.name == BuildExecutable.WINDOWS:
             os.system('pip install pyinstaller')
             os.system('pyinstaller src/main.py src/database/create_tables.py src/read_afd.py '+\
                       'src/models/_imports.py src/models/_models.py src/models/_openpyxl.py src/models/_peewee_orm.py -n ClockReader --clean --onefile')    
