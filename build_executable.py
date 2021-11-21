81699af5201afbcc0e59f0eda04569d1ac734673
saucesar <sau.cesarlima2013@gmail.com>
2021-11-21 15:32:00 -0300

    criação de pastas para log no processo de build

--- build_executable.py
+++ build_executable.py
@@ -23,12 +23,14 @@ class BuildExecutable:
         if os.name == BuildExecutable.LINUX:
             os.system('pip3 install pyinstaller')
             os.system('pyinstaller src/menu.py src/database/create_tables.py src/models/*.py -n ClockReader --noconsole --clean --onefile')
+            os.system('mkdir logs')
             os.system('mkdir -p dist/src/')
             os.system('cp -r src/assets dist/src/assets')
         elif os.name == BuildExecutable.WINDOWS:
             os.system('pip install pyinstaller')
             os.system('pyinstaller src/menu.py src/database/create_tables.py src/models/_imports.py src/models/_models.py '+\
                       'src/models/_openpyxl.py src/models/_peewee_orm.py -n ClockReader --noconsole --clean --onefile')
+            os.system('mkdir logs')
             os.system('mkdir dist/src/')
             os.system('copy src\\assets\\* dist\\src\\assets')
         else:
